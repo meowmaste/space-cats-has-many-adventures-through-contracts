fc98cdfc7536f5c8628d596d0a0c3e47bc26d54e
Joslyn <jrosbrook@gmail.com>
2014-01-30 15:09:18 -0800

    more style, add image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,4 @@ SpaceCatsAdventuresThroughContracts::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.reload_classes_only_on_change = false
-  config.middleware.delete Rack::Lock
 end
